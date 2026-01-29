b04ede79487d04b74d18fb1580c8aa3524c9dfd5
srkdev404-cyber <srkdev404@gmail.com>
2026-01-29 05:10:58 +0000

    chore: release WordPress plugin v0.0.0-alpha

--- admin/js/languages/pt_pt.js
+++ admin/js/languages/pt_pt.js
@@ -1,5 +1,5 @@
 /*!
- * froala_editor v4.7.1 (https://www.froala.com/wysiwyg-editor)
+ * froala_editor v0.0.0-alpha (https://www.froala.com/wysiwyg-editor)
  * License https://froala.com/wysiwyg-editor/terms/
  * Copyright 2014-2025 Froala Labs
  */
